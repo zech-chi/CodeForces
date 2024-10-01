fc3862ac82c70ef7da300ac29e5bfd6da80cd2c5
zech-chi <machinelearning1.618@gmail.com>
2024-10-01 13:50:51 +0100

    .

---
+++ A/122 Wizards' Duel.cpp
@@ -0,0 +1,38 @@
+/*
+* ███████╗███████╗ ██████╗██╗  ██╗       ██████╗██╗  ██╗██╗
+* ╚══███╔╝██╔════╝██╔════╝██║  ██║      ██╔════╝██║  ██║██║
+*   ███╔╝ █████╗  ██║     ███████║█████╗██║     ███████║██║
+*  ███╔╝  ██╔══╝  ██║     ██╔══██║╚════╝██║     ██╔══██║██║
+* ███████╗███████╗╚██████╗██║  ██║      ╚██████╗██║  ██║██║
+* ╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝       ╚═════╝╚═╝  ╚═╝╚═╝
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+
+void solve()
+{
+    int l, p, q; cin >> l >> p >> q;
+    cout << (1.0 * ((1.0 * l * p) /  (1.0 * (p + q)))) << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    return (0);
+}
+
