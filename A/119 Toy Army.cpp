7e6c8fcc43d411b1d832c4287f178b8ba85a2aaa
zech-chi <machinelearning1.618@gmail.com>
2024-10-01 12:08:01 +0100

    .

---
+++ A/119 Toy Army.cpp
@@ -0,0 +1,38 @@
+/*
+* ███████╗███████╗ ██████╗██╗  ██╗       ██████╗██╗  ██╗██╗
+* ╚══███╔╝██╔════╝██╔════╝██║  ██║      ██╔════╝██║  ██║██║
+*   ███╔╝ █████╗  ██║     ███████║█████╗██║     ███████║██║
+*  ███╔╝  ██╔══╝  ██║     ██╔══██║╚════╝██║     ██╔══██║██║
+* ███████╗███████╗╚██████╗██║  ██║      ╚██████╗██║  ██║██║
+* ╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝       ╚═════╝╚═╝  ╚═╝╚═╝
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+
+void solve()
+{
+    ll n; cin >> n;
+    cout << (n + n / 2) << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    return (0);
+}
+
