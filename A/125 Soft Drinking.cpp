1a27dd7c6dd23773a8222e798ef8987609b81fc4
zech-chi <machinelearning1.618@gmail.com>
2024-10-04 20:13:47 +0100

    .

---
+++ A/125 Soft Drinking.cpp
@@ -0,0 +1,43 @@
+/*
+* ███████╗███████╗ ██████╗██╗  ██╗       ██████╗██╗  ██╗██╗
+* ╚══███╔╝██╔════╝██╔════╝██║  ██║      ██╔════╝██║  ██║██║
+*   ███╔╝ █████╗  ██║     ███████║█████╗██║     ███████║██║
+*  ███╔╝  ██╔══╝  ██║     ██╔══██║╚════╝██║     ██╔══██║██║
+* ███████╗███████╗╚██████╗██║  ██║      ╚██████╗██║  ██║██║
+* ╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝       ╚═════╝╚═╝  ╚═╝╚═╝
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+
+void solve()
+{
+    int n, k, l, c, d, p, nl, np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+    int t1 = (l * k) / nl;
+    int t2 = p / np;
+    int t3 = c * d;
+    int res = min(t1, min(t2, t3)) / n;
+    cout << res << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    return (0);
+}
+
