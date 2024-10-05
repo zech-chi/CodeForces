c4c3436e3b089a71e4a176707671da7ac6410220
zech-chi <machinelearning1.618@gmail.com>
2024-10-05 12:33:17 +0100

    .

---
+++ A/128 Circle Line.cpp
@@ -0,0 +1,51 @@
+/*
+* ███████╗███████╗ ██████╗██╗  ██╗       ██████╗██╗  ██╗██╗
+* ╚══███╔╝██╔════╝██╔════╝██║  ██║      ██╔════╝██║  ██║██║
+*   ███╔╝ █████╗  ██║     ███████║█████╗██║     ███████║██║
+*  ███╔╝  ██╔══╝  ██║     ██╔══██║╚════╝██║     ██╔══██║██║
+* ███████╗███████╗╚██████╗██║  ██║      ╚██████╗██║  ██║██║
+* ╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝       ╚═════╝╚═╝  ╚═╝╚═╝
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+
+void solve()
+{
+    int n; cin >> n;
+    vector<int> v(n);
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+        sum += v[i];
+    }
+    int s, t; cin >> s >> t;
+    int mn = min(s - 1, t - 1);
+    int mx = max(s - 1, t - 1);
+    int s1 = 0;
+    for (int i = mn; i < mx; i++) {
+        s1 += v[i];
+    }
+    cout << min(s1, sum - s1) << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    return (0);
+}
+
