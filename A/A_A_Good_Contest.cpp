d2e968ba81a2b209169c0bbad3e6f639f69a054a
zech-chi <machinelearning1.618@gmail.com>
2024-10-27 17:42:20 +0100

    .

---
+++ A/A_A_Good_Contest.cpp
@@ -0,0 +1,50 @@
+/*
+*   zech-chi
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+#define abs(a) (((a) > 0) ? (a) : (-(a)))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) > (b)) ? (b) : (a))
+
+
+void solve()
+{
+    int n; cin >> n;
+    string name;
+    int before, after;
+    bool flag = false;
+
+    for (int i = 0; i < n; i++) {
+        cin >> name >> before >> after;
+        if (before >= 2400 && after > before)
+            flag = true;
+    }
+
+    if (flag)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    return (0);
+}
+
