0f6d6c8336ee36c4c2da866b4317749faf215ab1
zech-chi <machinelearning1.618@gmail.com>
2024-10-31 19:20:30 +0100

    .

---
+++ B_Bear_and_Strings.cpp
@@ -0,0 +1,65 @@
+/*
+*   zech-chi
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+#define abs(a) (((a) > 0) ? (a) : (-(a)))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) > (b)) ? (b) : (a))
+
+
+void solve()
+{
+    string s; cin >> s;
+    string b = "bear";
+    int bears = 0;
+    int i, j;
+    int left, right;
+    int l, r;
+    long long res = 0;
+
+    i = 0;
+    while (i < s.size()) {
+        j = 0;
+        l = i;
+        r = i;
+        while (i < s.size() && j < b.size() && s[i] == b[j]) {
+            i++;
+            j++;
+            r++;
+        }
+
+        if (j == b.size()) {
+            left = l - bears + 1;
+            right = s.size() - r + 1;
+            res += (left * right);
+            bears++;
+            i--;
+        }
+        i++;
+    }
+
+    cout << res << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    return (0);
+}
+
