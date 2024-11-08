ec7851e7eb436fe85c9bc9fe234f245413455952
zech-chi <machinelearning1.618@gmail.com>
2024-11-08 21:42:55 +0100

    .

---
+++ A/A_Quintomania.cpp
@@ -0,0 +1,57 @@
+/*
+*   zech-chi
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <map>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+#define abs(a) (((a) > 0) ? (a) : (-(a)))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) > (b)) ? (b) : (a))
+
+
+void solve()
+{
+    int t; cin >> t;
+
+    while (t--) {
+        int n; cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        bool flag = true;
+        for (int i = 1; i < n; i++) {
+            if (abs(a[i] - a[i - 1]) != 5 && abs(a[i] - a[i - 1]) != 7) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    return (0);
+}
+
