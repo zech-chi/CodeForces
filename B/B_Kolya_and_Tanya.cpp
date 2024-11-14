5dda6e120c11a948013f382089cbd4ec1a44b1a0
zech-chi <machinelearning1.618@gmail.com>
2024-11-14 11:32:45 +0100

    .

---
+++ B/B_Kolya_and_Tanya.cpp
@@ -0,0 +1,64 @@
+/*
+*   zech-chi
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <map>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+#define abs(a) (((a) > 0) ? (a) : (-(a)))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) > (b)) ? (b) : (a))
+
+const int MOD = 1e9 + 7;
+
+class Solution {
+private:
+
+public:
+
+    ll mod_pow(ll base, ll exp) {
+        ll res = 1;
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                res = (res * base) % MOD;
+            }
+            base = (base * base) % MOD;
+            exp /= 2;
+        }
+
+        return (res);
+    }
+
+    void solve() {
+        int n; cin >> n;
+        long long res = mod_pow(3, 3 * n);
+        res -= mod_pow(7, n);
+
+        if (res < 0)
+            res += MOD;
+
+        cout << res << "\n";
+    }
+
+};
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution().solve();
+    return (0);
+}
