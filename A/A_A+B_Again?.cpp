81f835cfabbdfce3befd735755eb1ba8d5ef0a19
zech-chi <machinelearning1.618@gmail.com>
2024-11-17 20:41:27 +0100

    .

---
+++ A/A_A+B_Again?.cpp
@@ -0,0 +1,52 @@
+/*
+*   zech-chi
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <map>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+#define abs(a) (((a) > 0) ? (a) : (-(a)))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) > (b)) ? (b) : (a))
+
+
+class Solution {
+private:
+
+public:
+    void solve() {
+		int t; cin >> t;
+		while (t--) {
+			int n; cin >> n;
+			int sum = 0;
+			while (n) {
+				sum += n % 10;
+				n /= 10;	
+			}
+
+			cout << sum << "\n";
+		}
+    }
+
+};
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution().solve();
+    return (0);
+}
