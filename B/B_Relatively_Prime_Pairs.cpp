197782d71c914d533409009562898174671435c0
zech-chi <machinelearning1.618@gmail.com>
2024-11-23 12:50:07 +0100

    .

---
+++ B/B_Relatively_Prime_Pairs.cpp
@@ -0,0 +1,49 @@
+/*
+*   zech-chi
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <queue>
+#include <map>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+#define abs(a) (((a) > 0) ? (a) : (-(a)))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) > (b)) ? (b) : (a))
+
+
+class Solution {
+private:
+
+public:
+    void solve() {
+		ll l, r; cin >> l >> r;
+
+		cout << "YES\n";
+		while (l <= r) {
+			cout << l << " " << l + 1 << "\n";
+			l += 2;
+		}
+    }
+
+};
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution().solve();
+    return (0);
+}
