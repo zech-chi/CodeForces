63d58a6232978a53c1e00e2910ab77f90906d695
zech-chi <machinelearning1.618@gmail.com>
2024-12-06 14:59:58 +0100

    .

---
+++ B/B_Wilbur_and_Array.cpp
@@ -0,0 +1,60 @@
+/*
+*   zech-chi
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <queue>
+#include <map>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+#define abs(a) (((a) > 0) ? (a) : (-(a)))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) > (b)) ? (b) : (a))
+
+
+class Solution {
+private:
+
+public:
+    void solve() {
+		int n; cin >> n;
+		int b, cur = 0;
+		ll steps = 0;
+
+		for (int i = 0; i < n; i++) {
+			cin >> b;
+			if (cur > b) {
+				steps += cur - b;
+				cur = b;
+			} else if (cur < b) {
+				steps += b - cur;
+				cur = b;
+			} else {
+				continue;
+			}
+		}
+		cout << steps << "\n";
+
+    }
+
+};
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution().solve();
+    return (0);
+}
