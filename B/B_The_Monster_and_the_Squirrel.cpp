b6ccf922d7fa2869340cba1691984cd3b839a4c9
zech-chi <machinelearning1.618@gmail.com>
2024-12-09 22:16:05 +0100

    .

---
+++ B/B_The_Monster_and_the_Squirrel.cpp
@@ -0,0 +1,46 @@
+/*
+*   zech-chi
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <queue>
+#include <map>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+#define abs(a) (((a) > 0) ? (a) : (-(a)))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) > (b)) ? (b) : (a))
+
+
+class Solution {
+private:
+
+public:
+    void solve() {
+		int n; cin >> n;
+
+		ll res = pow(n - 2, 2);
+		cout << res << "\n"; 
+    }
+
+};
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution().solve();
+    return (0);
+}
