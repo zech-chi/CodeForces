19fbae36524339c375db7de7d29f777a12a22492
zech-chi <machinelearning1.618@gmail.com>
2024-12-11 11:43:40 +0100

    .

---
+++ B/B_Ohana_Cleans_Up.cpp
@@ -0,0 +1,57 @@
+/*
+*   zech-chi
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <queue>
+#include <map>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+#define abs(a) (((a) > 0) ? (a) : (-(a)))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) > (b)) ? (b) : (a))
+
+
+class Solution {
+private:
+
+public:
+    void solve() {
+		int n; cin >> n;
+		map<string, int> mp;
+		string s;
+
+		for (int i = 0; i < n; i++) {
+			cin >> s;
+			mp[s]++;
+		}
+
+		int maxi = 1;
+		for (auto it = mp.begin(); it != mp.end(); it++) {
+			maxi = max(maxi, it->second);
+		}
+
+		cout << maxi << "\n";
+    }
+
+};
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution().solve();
+    return (0);
+}
