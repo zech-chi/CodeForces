5b027f520ddc8314b47c658c246a4a73274af63b
zech-chi <machinelearning1.618@gmail.com>
2024-12-25 21:42:03 +0100

    .

---
+++ B/B_Amr_and_Pins.cpp
@@ -0,0 +1,47 @@
+/*
+*   zech-chi
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <queue>
+#include <map>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+#define abs(a) (((a) > 0) ? (a) : (-(a)))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) > (b)) ? (b) : (a))
+
+
+class Solution {
+private:
+
+public:
+    void solve() {
+		int r, xs, ys, xe, ye; cin >> r >> xs >> ys >> xe >> ye;
+		double distance = sqrt(pow(xe - xs, 2) + pow(ye - ys, 2));
+		int res = ceil(distance / (2.0 * r));
+
+		cout << res << "\n";
+    }
+
+};
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution().solve();
+    return (0);
+}
