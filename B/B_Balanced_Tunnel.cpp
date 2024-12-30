d96eed72e3c7c7cd04db1beb0a649080925d8810
Zakaria Ech-chifaouy <zech-chi@e2r12p1.1337.ma>
2024-12-30 22:03:29 +0100

    .

---
+++ B/B_Balanced_Tunnel.cpp
@@ -0,0 +1,64 @@
+/*
+*   zech-chi
+*/
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <queue>
+#include <map>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <stack>
+#include <iomanip>
+typedef long long ll;
+using namespace std;
+#define abs(a) (((a) > 0) ? (a) : (-(a)))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) > (b)) ? (b) : (a))
+
+
+class Solution {
+private:
+
+public:
+    void solve() {
+        int n; cin >> n;
+        vector<int> a(n), b(n);
+
+        for (int i = 0; i < n; i++) cin >> a[i];
+        for (int i = 0; i < n; i++) cin >> b[i];
+
+        int i = 0, j = 0;
+        set<int> seen;
+
+        while (i < n && j < n) {
+            if (a[i] == b[j]) {
+                i++;
+                j++;
+            } else if (seen.find(a[i]) != seen.end()) {
+                i++;
+            } else {
+                seen.insert(b[j]);
+                j++;
+            }
+        }
+
+        cout << seen.size() << "\n";
+    }
+
+};
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution().solve();
+    return (0);
+}
